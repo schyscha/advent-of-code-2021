51f98a379a7a7a9282a8974e78b8d56fa1edd78a
Szymon Rozmarynowski <szymonrozmarynowski@gmail.com>
2021-12-11 01:56:42 +0100

    [10] Solutions for both tasks from day 10 added

---
+++ Day10/src/aoc2021/exec/Task1.java
@@ -0,0 +1,19 @@
+package aoc2021.exec;
+
+import aoc2021.impl.SyntaxErrorCalculator;
+
+/**
+ * Advent of Code 2021
+ * Day 10
+ * <p>
+ * Execution of the solution for the task 01
+ *
+ * @author Szymon Rozmarynowski
+ */
+public class Task1 {
+
+    public static void main(String[] args) {
+        SyntaxErrorCalculator syntaxErrorCalculator = new SyntaxErrorCalculator();
+        syntaxErrorCalculator.calculate();
+    }
+}
---
+++ Day10/src/aoc2021/exec/Task2.java
@@ -0,0 +1,19 @@
+package aoc2021.exec;
+
+import aoc2021.impl.IncompleteErrorCalculator;
+
+/**
+ * Advent of Code 2021
+ * Day 10
+ * <p>
+ * Execution of the solution for the task 02
+ *
+ * @author Szymon Rozmarynowski
+ */
+public class Task2 {
+
+    public static void main(String[] args) {
+        IncompleteErrorCalculator incompleteErrorCalculator = new IncompleteErrorCalculator();
+        incompleteErrorCalculator.calculate();
+    }
+}
